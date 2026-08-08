159fc61047bdf47d82d2c8de71189af51fb1c163
Luka Nizharadze <luka.nizharadze@citypay.io>
2026-08-08 17:19:06 +0400

    initialize Spring Boot project

---
+++ src/test/java/com/lukanizharadze/minibanking/MiniBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukanizharadze.minibanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
